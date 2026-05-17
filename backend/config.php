614286624fd6a1556c7d2b661c608cbc3bcfded0
iballacarlo <carlo.iballa@cvsu.edu.ph>
2026-05-18 01:02:14 +0800

    ayusin mo yung delete ng complaints/requests

--- backend/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Edit these values for your MySQL setup
-return [
-  'db_host' => 'localhost',     // MySQL host
-  'db_name' => 'barangay_db',   // Database name
-  'db_user' => 'root',          // MySQL username (default)
-  'db_pass' => '',              // MySQL password (empty by default in XAMPP)
-];
